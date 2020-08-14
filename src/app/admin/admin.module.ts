9f9191479e1725118dd59cd3e6a23dbb324e99c0
Gustavo Andres Romero Ordoñez <gus.romero.ordonez@outlook.com>
2020-08-14 03:32:25 -0500

    was added validation on the form of create products

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../material/material.module';
+import { NgxCurrencyModule } from 'ngx-currency';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { ProductFormComponent } from './components/product-form/product-form.component';
@@ -17,6 +19,8 @@ import { StockComponent } from './components/stock/stock.component';
     AdminRoutingModule,
     ReactiveFormsModule,
     MaterialModule,
+    NgxCurrencyModule,
+    SweetAlert2Module
   ],
 })
 export class AdminModule { }
--- src/app/admin/components/product-form/product-form.component.ts
+++ src/app/admin/components/product-form/product-form.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, NgForm } from '@angular/forms';
+
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-product-form',
@@ -8,9 +11,10 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 
 export class ProductFormComponent {
+  @ViewChild('formDirective') private formDirective: NgForm;
   productForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private router: Router) {
     this.buildForm();
   }
 
@@ -36,10 +40,7 @@ export class ProductFormComponent {
         ]
       ],
       image: [
-        null,
-        [
-          Validators.required
-        ]
+        null
       ],
       description: [
         null,
@@ -50,10 +51,33 @@ export class ProductFormComponent {
     });
   }
 
+  private resetForm(): void {
+    this.buildForm();
+    this.formDirective.resetForm();
+  }
+
   onSubmit(e: Event): void {
     e.preventDefault();
     if (this.productForm.valid) {
-      alert('Thanks!');
+      Swal.fire({
+        title: 'El producto fue registrado con exito',
+        icon: 'success',
+        showCancelButton: true,
+        confirmButtonText: 'Ir al inventario',
+        cancelButtonText: 'Crear otro producto',
+        customClass: {
+          confirmButton: 'mat-focus-indicator mat-raised-button mat-button-base mat-primary mr-1',
+          cancelButton: 'mat-focus-indicator mat-raised-button mat-button-base'
+        },
+        buttonsStyling: false,
+        allowOutsideClick: false
+      }).then((r) => {
+        if (r.value) {
+          this.router.navigate(['./admin/stock']);
+        } else {
+          this.resetForm();
+        }
+      });
     }
   }
 }
