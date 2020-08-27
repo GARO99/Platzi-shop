0e0c1237e9f82bca39ee54c4bfe92c32b9eed1ca
Gustavo Andres Romero Ordoñez <gus.romero.ordonez@outlook.com>
2020-08-26 23:01:22 -0500

    Advance in the code to sign up page

--- src/app/admin/auth/components/register/register.component.ts
+++ src/app/admin/auth/components/register/register.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CustomValidators } from 'src/app/util/custom-validators';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  @ViewChild('formDirective') private formDirective: NgForm;
+  signUpForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.FormBuild();
+   }
 
   ngOnInit(): void {
   }
 
+  private FormBuild(): void{
+    this.signUpForm = this.fb.group({
+      fullName: [null, [Validators.required, CustomValidators.noEmpty]],
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required]],
+      confirmPassword: [null, [Validators.required]]
+    });
+  }
+
+  onSubmit(e: Event): void{
+    e.preventDefault();
+  }
 }
