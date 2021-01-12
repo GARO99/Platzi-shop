a4a78796dd8b26e3e77d030befb499b454eda4d1
Gustavo Andres Romero Ordoñez <gus.romero.ordonez@outlook.com>
2021-01-12 00:41:55 -0500

    The unit testing was implemented in product service

--- src/app/core/services/products/products.service.spec.ts
+++ src/app/core/services/products/products.service.spec.ts
@@ -1,16 +1,58 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ProductsService } from './products.service';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '@environments/environment';
 
-xdescribe('ProductsService', () => {
+describe('ProductsService', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
   let service: ProductsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(ProductsService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('test for getAllProducts', () => {
+    it('should return array products', () => {
+      const expectData = [
+        {
+          id: '1',
+          image: 'img/img.jpg',
+          title: 'Asdf',
+          price: 1212,
+          description: 'asdasdasd'
+        },
+        {
+          id: '2',
+          image: 'img/img.jpg',
+          title: 'Asdf',
+          price: 1212,
+          description: 'asdasdasd'
+        }
+      ];
+      let dataError: any[];
+      let dataResponse: any[];
+      service.getAllProducts().subscribe(r => {
+        dataResponse = r;
+      }, error => {
+        dataError = error;
+      });
+      const request = httpTestingController.expectOne(`${environment.urlAPI}products`);
+      request.flush(expectData);
+      expect(dataResponse.length).toEqual(2);
+      expect(request.request.method).toEqual('GET');
+      expect(dataError).toBeUndefined();
+    });
+  });
+
 });
